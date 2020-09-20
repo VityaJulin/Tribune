b980f7b6d9f8602025912633db373597c56f8710
vityajulin <vkdvornokov@gmail.com>
2020-09-20 20:33:52 +0300

    Initial commit

---
+++ app/src/main/java/com/example/tribune/activity/Statistic.kt
@@ -0,0 +1,12 @@
+package com.example.tribune.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tribune.R
+
+class Statistic : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_statistic)
+    }
+}
