008e93bcfcbc06af32e96ac3b1c4310fd3976186
vityajulin <vkdvornikov@gmail.com>
2020-09-22 20:16:35 +0300

    fix view names

--- app/src/main/java/com/example/tribune/activity/Registeration.kt
+++ app/src/main/java/com/example/tribune/activity/Registeration.kt
@@ -3,10 +3,17 @@ package com.example.tribune.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.tribune.R
+import kotlinx.android.synthetic.main.activity_registeration.*
 
 class Registeration : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registeration)
     }
+
+    val registerBtn = registration_btn_register
+    val haveAccountBtn = registration_btn_have_account
+    val userNameEdTxt = registration_edtxt_username
+    val userPassword = registration_edtxt_password
+    val confirnPassword = registration_edtxt_confirm
 }
